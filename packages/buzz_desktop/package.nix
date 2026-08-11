a56bcff4730cf3622fa1b5f6672b5e0d7e1cf66a
ThurgyThurg <tim@graham29.com>
2026-08-11 13:04:28 -0400

    Update package.nix

--- packages/buzz_desktop/package.nix
+++ packages/buzz_desktop/package.nix
@@ -1,38 +1,111 @@
 {
-  appimageTools,
-  fetchurl,
   lib,
+  stdenvNoCC,
+  fetchurl,
+  dpkg,
+  buildFHSEnv,
   pkgs,
 }:
 
 let
-  pname = "buzz";
   version = "0.5.9";
 
-  src = fetchurl {
-    url = "https://github.com/block/buzz/releases/download/desktop-v${version}/Buzz_${version}_amd64.AppImage";
-    # Run `nix build`, it will fail and print the correct hash to paste here.
-    hash = sha256:5af4c03db35385c561a99564458b445b6659c252c1ac72daf2a1c85a0c0a4a27;
+  buzz-unwrapped = stdenvNoCC.mkDerivation {
+    pname = "buzz-unwrapped";
+    inherit version;
+    src = fetchurl {
+      url = "https://github.com/block/buzz/releases/download/desktop-v${version}/Buzz_${version}_amd64.deb";
+      hash = "sha256:400b00fc410b1fdeed66f30886e7d52f14a021315e29d632dcc0a28e18021c71";
+    };
+    nativeBuildInputs = [ dpkg ];
+    unpackPhase = "dpkg-deb -x $src .";
+    dontConfigure = true;
+    dontBuild = true;
+    dontStrip = true;
+    dontPatchELF = true;
+    installPhase = ''
+      runHook preInstall
+      mkdir -p $out
+      cp -r usr/. $out/
+      runHook postInstall
+    '';
   };
-
-  appimageContents = appimageTools.extractType2 { inherit pname version src; };
 in
-appimageTools.wrapType2 {
-  inherit pname version src;
+buildFHSEnv {
+  name = "buzz";
+  runScript = "${buzz-unwrapped}/bin/buzz-desktop";
 
-  extraPkgs = pkgs: with pkgs; [
-    elfutils
-    zstd
+  profile = ''
+    export GST_PLUGIN_SYSTEM_PATH_1_0=/usr/lib/gstreamer-1.0
+    export WEBKIT_DISABLE_DMABUF_RENDERER=1
+  '';
+
+  targetPkgs = pkgs: with pkgs; [
+    webkitgtk_4_1
+    gtk3
+    glib
+    glib-networking
+    libsoup_3
+    cairo
+    pango
+    gdk-pixbuf
+    harfbuzz
+    librsvg
+    atk
+    at-spi2-atk
+    at-spi2-core
+    gsettings-desktop-schemas
+    dconf
+    openssl
+    zlib
+    curl
+    nss
+    nspr
+    gst_all_1.gstreamer
     gst_all_1.gst-plugins-base
     gst_all_1.gst-plugins-good
-    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-libav
+    gst_all_1.gst-plugins-ugly
+    libGL
+    libglvnd
+    mesa
+    libdrm
+    xorg.libX11
+    xorg.libXext
+    xorg.libXrender
+    xorg.libXi
+    xorg.libXcursor
+    xorg.libXdamage
+    xorg.libXfixes
+    xorg.libXcomposite
+    xorg.libXrandr
+    xorg.libXtst
+    xorg.libxcb
+    wayland
+    libxkbcommon
+    fontconfig
+    freetype
+    alsa-lib
+    libpulseaudio
+    pipewire
+    dbus
+    libnotify
+    expat
+    libffi
+    pcre2
   ];
 
   extraInstallCommands = ''
-    install -m 444 -D ${appimageContents}/Buzz.desktop $out/share/applications/${pname}.desktop
-    substituteInPlace $out/share/applications/${pname}.desktop \
-      --replace-fail 'Exec=buzz-desktop' 'Exec=${pname}'
-    cp -r ${appimageContents}/usr/share/icons $out/share
+    mkdir -p $out/share
+    cp -r ${buzz-unwrapped}/share/applications $out/share/ 2>/dev/null || true
+    cp -r ${buzz-unwrapped}/share/icons $out/share/ 2>/dev/null || true
+    for d in $out/share/applications/*.desktop; do
+      [ -e "$d" ] || continue
+      substituteInPlace "$d" \
+        --replace-quiet "Exec=buzz-desktop" "Exec=buzz" \
+        --replace-quiet "Exec=/usr/bin/buzz-desktop" "Exec=buzz"
+    done
   '';
 
   meta = {
