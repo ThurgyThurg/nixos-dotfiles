5059b3903d223911c8280d9c2cc7082b2944ac5b
ThurgyThurg <tim@graham29.com>
2026-08-20 20:33:32 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -46,14 +46,12 @@
             (final: prev: {
               donetick-tui = donetick-tui.packages.${prev.system}.default;
             })
-          ];
         }
         ./hosts/P52
         nixos-hardware.nixosModules.lenovo-thinkpad-p52
         agenix.nixosModules.default
         home-manager.nixosModules.home-manager
         openlogi.nixosModules.default
-          { programs.openlogi.enable = true; }
         {
           home-manager = {
             useGlobalPkgs = true;
@@ -70,7 +68,6 @@
         ./hosts/itx
         agenix.nixosModules.default
         openlogi.nixosModules.default
-          { programs.openlogi.enable = true; }
         home-manager.nixosModules.home-manager
         {
           home-manager = {
