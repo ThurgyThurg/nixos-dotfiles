475b69bb63e0079e867c9c0c16fd72e3ab996610
ThurgyThurg <tim@graham29.com>
2026-08-21 13:30:34 -0400

    Create common-system.nix

---
+++ modules/common-system.nix
@@ -0,0 +1,92 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  boot.loader.limine.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.limine.maxGenerations = 20;
+  services.getty.autologinUser = "tim";
+
+  boot.initrd.systemd.enable = true;
+  boot.initrd.luks.devices."encrypted" = {
+    crypttabExtraOpts = ["fido2-device=auto"];
+  };
+
+  time.timeZone = "America/New_York";
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  age.identityPaths = ["/home/tim/.age/agenix-identity.txt"];
+  age.secrets.github-token = {
+    file = ../secrets/github-token.age;
+    owner = "tim";
+    group = "users";
+    mode = "0400";
+  };
+  age.secrets.donetick-token = {
+    file = ../secrets/donetick-token.age;
+    owner = "tim";
+    group = "users";
+    mode = "0400";
+  };
+  age.secrets.cf-access-client-secret = {
+    file = ../secrets/cf-access-client-secret.age;
+    owner = "tim";
+    group = "users";
+    mode = "0400";
+  };
+
+  networking.networkmanager.enable = true;
+  services.tailscale.enable = true;
+
+  services.xserver = {
+    enable = true;
+    windowManager.oxwm.enable = true;
+  };
+
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+  users.users.tim = {
+    isNormalUser = true;
+    extraGroups = ["wheel"];
+    packages = with pkgs; [tree];
+  };
+
+  services.gvfs.enable = true;
+  services.tumbler.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    neovim
+    wget
+    nano
+    impala
+    nfs-utils
+    rofi
+    sbctl
+    alacritty
+    feh
+    thunar
+    thunar-volman
+    thunar-archive-plugin
+    picom
+    dunst
+    unzip
+    onlyoffice-desktopeditors
+    donetick-tui
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  programs.firefox.enable = true;
+  programs.dconf.enable = true;
+  programs.i3lock.enable = true;
+
+  fonts.packages = with pkgs; [nerd-fonts.jetbrains-mono];
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  system.stateVersion = "25.11";
+}
