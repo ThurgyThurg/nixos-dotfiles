590b6f6786fb3dc322ff85a5f81793d2658e289a
ThurgyThurg <tim@graham29.com>
2026-08-22 10:24:39 -0400

    Prepare for fusion attempt

--- hosts/itx/default.nix
+++ hosts/itx/default.nix
@@ -16,6 +16,7 @@
   environment.systemPackages = with pkgs; [
     steam-run
     cifs-utils
+    protonup-qt
   ];
 
   programs.steam = {
--- modules/common-home.nix
+++ modules/common-home.nix
@@ -43,6 +43,7 @@ in {
     redshift
     pavucontrol
     buzz
+    rustdesk-flutter
     (pkgs.writeShellApplication {
       name = "ns";
       runtimeInputs = with pkgs; [
@@ -87,7 +88,7 @@ in {
       if [ -r /run/agenix/donetick-token ]; then
         export DONETICK_TOKEN="$(cat /run/agenix/donetick-token)"
       fi
-      export DONETICK_URL="https://tasks.graham29.com"
+      export DONETICK_URL="C"
       if [ -r /run/agenix/cf-access-client-secret ]; then
         export CF_ACCESS_CLIENT_SECRET="$(cat /run/agenix/cf-access-client-secret)"
       fi
