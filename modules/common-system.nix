4c959c0b241265eda064665c7612525e30a9951b
ThurgyThurg <tim@graham29.com>
2026-08-25 11:36:01 -0400

    Update common-system.nix

--- modules/common-system.nix
+++ modules/common-system.nix
@@ -50,9 +50,6 @@
   services.xserver = {
     enable = true;
     windowManager.oxwm.enable = true;
-    displayManager.sessionCommands = ''
-      xset s 5400 dpms 5400 5400 5400
-    '';
   };
 
   services.pipewire = {
