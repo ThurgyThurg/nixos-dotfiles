abf7d55e1bb1ee89cca2441429d2eb83f42095c6
ThurgyThurg <tim@graham29.com>
2026-09-20 11:10:01 -0400

    Update common-home.nix

--- modules/common-home.nix
+++ modules/common-home.nix
@@ -20,6 +20,7 @@ in {
   home.homeDirectory = "/home/tim";
   home.stateVersion = "25.11";
   home.file.".xinitrc".source = create_symlink "${dotfiles}/xinitrc";
+  home.file.".local/share/fonts/Anton-Regular.ttf".source = ../Anton-Regular.ttf;
   home.file.".config/openlogi".source =
     config.lib.file.mkOutOfStoreSymlink
     "${config.home.homeDirectory}/nixos-dotfiles/config/openlogi";
