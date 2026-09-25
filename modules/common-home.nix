3aa217c416bffe840e25818ae756c13687351d38
ThurgyThurg <tim@graham29.com>
2026-09-25 00:39:03 -0400

    Update common-home.nix

--- modules/common-home.nix
+++ modules/common-home.nix
@@ -48,7 +48,9 @@ in {
     redshift
     pavucontrol
     remmina
-    freecad
+    (freecad.customize {
+      pythons = [ (ps: with ps; [ pyjwt requests tzlocal ]) ];
+    })
     davinci-resolve
     obs-studio
     bluebubbles
